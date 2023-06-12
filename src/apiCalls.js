715207c8ffb0f4c6eb6fb460bd1726ac6864aed8
Joshua Martin <ursa8minority@gmail.com>
2023-06-12 09:51:58 -0600

    API network request successful and displaying on DOM

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,31 @@
-export const getUrls = () => {
+const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+  .then(response => {
+    if(!response.ok){
+      throw new Error("Failed to Fetch Urls")
+    }
+    return response.json()
+    })
+      
 }
+
+// export const postUrl = (data) => {
+//   // {long_url: <String>, title: <String>}
+//   fetchfetch('http://localhost:3001/api/v1/urls')
+// }
+
+
+
+
+
+export default getUrls;
+// // Get all saved urls	
+// /api/v1/urls	GET	N/A	All urls on the server: {urls: [{}, {}, ...]}
+
+// // Save a new url and get back a the shortened url	
+// /api/v1/urls	POST	{long_url: <String>, title: <String>}	New url that was added with the shortened url: 
+
+// {id: 2, long_url: "https://images.unsplash.com/photo...", short_url: "http://localhost:3001/useshorturl/2", title: 'Awesome photo'}
+
+// // Delete existing saved url	
+// /api/v1/urls/:url_id	DELETE	N/A	For successful deletion: No response body (only 204 status code)
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import { getUrls } from '../../apiCalls';
+import apiCalls from '../../apiCalls';
+import getUrls from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
 import UrlForm from '../UrlForm/UrlForm';
 
+
 export class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       urls: []
     }
   }
 
   componentDidMount() {
+    getUrls()
+    .then(data =>{
+      console.log(data)
+      this.setState({urls:[...data.urls]})
+    })
+    .catch(() => this.setState({error:"Loading Error!!"}))
   }
 
   render() {
